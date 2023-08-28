3efd1370ef373e3989ad870b134318b4baec78f1
unknown <jeewanthamsenanayake@gmail.com>
2023-08-28 18:41:56 +0530

    create account

---
+++ models/account.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    
+    
+    
+    
+    
+    
+    
+    
+    id: {
+        required: true,
+        type: String
+    },
+    fname: {
+        required: true,
+        type: String
+    },
+    lname: {
+        required: true,
+        type:String
+    },
+    address: {
+        required: true,
+        type: String
+    },
+    contact: {
+        required: true,
+        type: String
+    },
+    email: {
+        required: true,
+        type: String
+    },
+    birth: {
+        required: true,
+        type: String
+    },
+    gender: {
+        required: true,
+        type: String
+    },
+    location: {
+        required: true,
+        type: String
+    },
+    category: {
+        required: false,
+        type: String
+    },
+    descrip: {
+        required: true,
+        type: String
+    },
+    role: {
+        required: true,
+        type: String
+    },
+    pImgUrl: {
+        required: false,
+        type: String
+    },
+    workImgUrl: {
+        required: false,
+        type: Array
+    }
+})
+
+module.exports = mongoose.model('accounts', dataSchema)
---
+++ routes/account.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcrypt');
+
+const express = require('express');
+const Model = require('../models/account');
+
+
+
+const accountRouters = express.Router()
+accountRouters.post('/create_account', async (req, res) => {
+
+    const data = new Model({
+        fname: req.body.fname,
+        lname: req.body.lname,
+        address: req.body.address,
+        contact: req.body.contact,
+        email: req.body.email,
+        birth: req.body.birth,
+        gender: req.body.gender,
+        location: req.body.location,
+        category: req.body.category,
+        id: req.body.id,
+        role: req.body.role
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json({ })
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+
+});
+
+module.exports = accountRouters;
