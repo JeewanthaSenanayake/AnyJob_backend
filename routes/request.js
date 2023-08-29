02224e190bfedc8be37d47b0f27ebbc8a665516e
unknown <jeewanthamsenanayake@gmail.com>
2023-08-29 17:34:31 +0530

    update

--- routes/request.js
+++ routes/request.js
@@ -21,6 +21,29 @@ requestRout.post('/make_request', async (req, res) => {
 
 })
 
+requestRout.post('/update_request', async (req, res) => {
+    await Model.find({ woker_id: req.body.woker_id, requested: "1", cus_id: req.body.cus_id })
+        .exec()
+        .then(async (data) => {
+                let val = data[0];
+                val.requested = req.body.requested;
+            try {
+                const dataToSave = await val.save();
+                res.status(200).json({ "id": dataToSave._id })
+            }
+            catch (error) {
+                res.status(400).json({ message: error.message })
+            }
+
+        })
+        .catch((err) => {
+            // Handle error
+            res.status(500).json({ error: 'Internal server error' });
+        });
+})
+
+
+
 requestRout.get(`/admin_notification/:id`, async (req, res) => {
     const id = req.params.id;
 
@@ -28,16 +51,14 @@ requestRout.get(`/admin_notification/:id`, async (req, res) => {
         .exec()
         .then(async (data) => {
 
-            console.log(data);
-            
-            let finalData=[];
+            // console.log(data);
+
+            let finalData = [];
             for (const iterator of data) {
                 await AccountModel.find({ id: iterator.cus_id })
                     .exec()
                     .then(async (udata) => {
-                        
-                        udata[0].request_ref = data._id;
-                        console.log(udata[0])
+
                         finalData.push(udata[0])
                     })
             }
