c3fd2bc1c797c874f1e8c250056160882ca07c38
unknown <jeewanthamsenanayake@gmail.com>
2023-08-30 15:06:17 +0530

    login api

--- routes/authRouters.js
+++ routes/authRouters.js
@@ -52,52 +52,53 @@ authRouters.post('/login', async (req, res) => {
         .then(async (data) => {
             // Return the matching data
 
-            for (let i = 0; i < data.length; i++) {
-                console.log(i)
-                console.log(data.length - 1)
-                let isMatch = false;
-                bcrypt.compare(enteredPassword, data[i].password, (err, result) => {
-                    if (err) {
-                        // Handle error
-                        return;
-                    }
+            if (data.length == 0) {
+                res.status(404).json({ error: 'Data not found' });
+            } else {
 
-                    if (result) {
-                        // Passwords match
-                        console.log('Passwords match');
-                        // console.log(data[i]);
-                        dataFound = false;
-                        isMatch = true;
-                        finalData = {
-                            "id": data[i]._id,
-                            "uname": data[i].uname,
-                            "role": data[i].role
-                        }
-                        
-                       
-
-
-                    } else {
-                        // Passwords do not match
-                        console.log('Passwords do not match');
-                    }
-                    if (i == data.length - 1) {
-                        if (dataFound) {
-                            console.log("Data not found")
-                            res.status(404).json({ error: 'Data not found' });
-                        }else{
-                            res.status(200).json(finalData)
+                for (let i = 0; i < data.length; i++) {
+                    console.log(i)
+                    console.log(data.length - 1)
+                    let isMatch = false;
+                    bcrypt.compare(enteredPassword, data[i].password, (err, result) => {
+                        if (err) {
+                            // Handle error
+                            return;
                         }
 
-                    }
-                });
+                        if (result) {
+                            // Passwords match
+                            console.log('Passwords match');
+                            // console.log(data[i]);
+                            dataFound = false;
+                            isMatch = true;
+                            finalData = {
+                                "id": data[i]._id,
+                                "uname": data[i].uname,
+                                "role": data[i].role
+                            }
 
 
+
+
+                        } else {
+                            // Passwords do not match
+                            console.log('Passwords do not match');
+                        }
+                        if (i == data.length - 1) {
+                            if (dataFound) {
+                                console.log("Data not found")
+                                res.status(404).json({ error: 'Data not found' });
+                            } else {
+                                res.status(200).json(finalData)
+                            }
+
+                        }
+                    });
+
+
+                }
             }
-
-
-
-
         })
         .catch((err) => {
             // Handle error
